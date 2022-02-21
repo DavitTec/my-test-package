343ecf3608741aa03abbfd403ea4f67167303cae
Davit Technologies <david@davit.ie>
2022-02-21 22:49:14 +0100

    Create my-components.js

---
+++ src/my-components.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class Foo extends React.Component {
+  render() {
+    return <div>foo</div>;
+  }
+}
+
+export class Bar extends React.Component {
+  render() {
+    return <div>bar</div>;
+  }
+}
